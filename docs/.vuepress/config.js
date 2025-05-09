184b54449367ab1f2f89bca7e0e5b1e6eeb59fee
loopbin <lzb100562@163.com>
2025-05-09 18:20:49 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
-
+  base: 'logbook',
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
 
